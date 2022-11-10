6bc6ec6d5c8746eb24698508c9017bba135685be
Masikaomondi <108226877+Masikaomondi@users.noreply.github.com>
2022-11-10 12:38:06 +0300

    Add files via upload

---
+++ udacity.js
@@ -0,0 +1,10 @@
+console.log("Hiya friends!");
+console.log(10 * 2 + 24 - 2);
+var joke = "Why couldn't the shoes go out and play? \n They were all \"tied\" up\!"
+console.log(joke);
+
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+const myLine = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+//concatenation of two strings
+const ourStr = "This is the start. " + "This is the end. ";
